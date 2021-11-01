7f73f3ad3dbf4e41275597da0ba07c7aa6df8102
Dolen Ch. Deori <dulench.deori564@gmail.com>
2021-11-01 12:34:50 +0530

    navigation reset

--- js/index.js
+++ js/index.js
@@ -2,14 +2,10 @@
 
 const linkArea = document.querySelector(".nav--link--area");
 const toggleBtn = document.querySelector(".toggle--bars");
-const nav = document.querySelector("nav");
-const smallNav = document.querySelector(".navigation--area");
 
 window.addEventListener("load", function () {
   // NAVIGATION TOGGLE
   toggleBtn.addEventListener("click", () => {
     linkArea.classList.toggle("show");
-    nav.classList.toggle("reset");
-    smallNav.classList.toggle("reset--small");
   });
 });
