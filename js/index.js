2ad0c71775c68b6801da39b7216f1aba78849c0c
Dolen Ch. Deori <dulench.deori564@gmail.com>
2021-11-01 21:13:35 +0530

    update responsive navigation

--- js/index.js
+++ js/index.js
@@ -3,11 +3,15 @@
 const linkArea = document.querySelector(".nav--link--area");
 const toggleBtn = document.querySelector(".toggle--bars");
 const nav = document.querySelector("nav");
+const socialIcon = document.querySelectorAll(".social--icon");
 
 window.addEventListener("load", function () {
   // NAVIGATION TOGGLE
   toggleBtn.addEventListener("click", () => {
     linkArea.classList.toggle("show");
     nav.classList.toggle("overlay-nav");
+    for (let icon of socialIcon) {
+      icon.classList.toggle("social--icon--animate");
+    }
   });
 });
