a2c3cc037dd4f2cd73274a1a852431adade5541f
Dolen Ch. Deori <dulench.deori564@gmail.com>
2021-11-01 18:02:40 +0530

    update navigation collapse

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@
 
 const linkArea = document.querySelector(".nav--link--area");
 const toggleBtn = document.querySelector(".toggle--bars");
+const nav = document.querySelector("nav");
 
 window.addEventListener("load", function () {
   // NAVIGATION TOGGLE
   toggleBtn.addEventListener("click", () => {
     linkArea.classList.toggle("show");
+    nav.classList.toggle("overlay-nav");
   });
 });
