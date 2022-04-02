910b8cc59cb0af266d0744760a0f40239fd4e3d5
DolenDeori <dulench.deori564@gmail.com>
2022-04-03 02:52:15 +0530

    update contact button function

--- js/index.js
+++ js/index.js
@@ -28,10 +28,4 @@ window.addEventListener("load", function () {
   // AOS
   AOS.init();
 
-  let tooltipTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  );
-  let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-  });
 });
