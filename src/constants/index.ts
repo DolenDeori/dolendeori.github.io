f4c455cff2faddc71dd476b8a8e80f565cb0a1c8
DolenDeori <dulench.deori564@gmail.com>
2025-06-19 00:14:48 +0530

    feat: Add new SVG icons for skils section; update constants to include new images and icons, and enhance project and service details.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,17 +1,157 @@
 import dolendeori_image from "../assets/images/dolendeori_image.webp";
 import code_circle from "../assets/images/CodeCircle.png";
+import digixal_project_banner from "../assets/images/Digixal_logo_project.webp";
+
+import react from "../assets/icons/reactjs-icon.svg";
+import next from "../assets/icons/Nextjs.svg";
+import github from "../assets/icons/GitHub.svg";
+import git from "../assets/icons/Git.svg";
+import python from "../assets/icons/Python.svg";
+import django from "../assets/icons/Django.svg";
+import javascript from "../assets/icons/JavaScript.svg";
+import postman from "../assets/icons/Postman.svg";
+import tailwind from "../assets/icons/Tailwind CSS.svg";
+import figma from "../assets/icons/Figma.svg";
 
 export const images = {
   dolendeori_image,
   code_circle,
+  digixal_project_banner,
+};
+
+export const icons = {
+  react,
+  next,
+  github,
+  git,
+  python,
+  django,
+  javascript,
+  postman,
+  tailwind,
+  figma,
 };
 
 export const testimonials = [
   {
     name: "Dolen Deori",
-    Company: "Dolen Designs",
+    company: "Dolen Designs",
     message:
       "“Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries”",
     profile: images.dolendeori_image,
+  },
+  {
+    name: "Rubul Hazorika",
+    company: "Digixal Records",
+    message:
+      "“Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries”",
+    profile: images.dolendeori_image,
+  },
+  {
+    name: "Chinmoyee Bejboruah",
+    company: "Dolen Designs",
+    message:
+      "“Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries”",
+    profile: images.dolendeori_image,
+  },
+  {
+    name: "Dolen Deori",
+    company: "Dolen Designs",
+    message:
+      "“Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries”",
+    profile: images.dolendeori_image,
+  },
+];
+
+export const projects = [
+  {
+    title: "DigiXal Logo Design",
+    banner: images.digixal_project_banner,
+    techUsed: ["react.js", "node.js", "adobe illustrator"],
+    date: "20-07-2003",
+    type: "Graphics Designing",
+  },
+  {
+    title: "DigiXal Logo Design",
+    banner: images.digixal_project_banner,
+    techUsed: ["react.js", "node.js", "adobe illustrator"],
+    date: "20-07-2003",
+    type: "Graphics Designing",
+  },
+  {
+    title: "DigiXal Logo Design",
+    banner: images.digixal_project_banner,
+    techUsed: ["react.js", "node.js", "adobe illustrator"],
+    date: "20-07-2003",
+    type: "Graphics Designing",
+  },
+  {
+    title: "DigiXal Logo Design",
+    banner: images.digixal_project_banner,
+    techUsed: ["react.js", "node.js", "adobe illustrator"],
+    date: "20-07-2003",
+    type: "Graphics Designing",
+  },
+];
+
+export const services = [
+  {
+    service: "Web Development",
+    desc: "I build clean, fast, and responsive websites that work smoothly across all devices and help you grow online.",
+  },
+  {
+    service: "Shopify",
+    desc: "Get a fully functional, customized Shopify store that’s easy to manage and designed to convert visitors into customers.",
+  },
+  {
+    service: "Logo Designing",
+    desc: "I create simple, memorable logos that reflect your brand’s identity and make a strong first impression.",
+  },
+  {
+    service: "App Development",
+    desc: "From idea to launch, I develop mobile apps that are user-friendly, reliable, and tailored to your business needs.",
+  },
+];
+
+export const skills = [
+  {
+    skill: "React.js",
+    icon: icons.react,
+  },
+  {
+    skill: "Next.js",
+    icon: icons.next,
+  },
+  {
+    skill: "JavaScript",
+    icon: icons.javascript,
+  },
+  {
+    skill: "React Native",
+    icon: icons.react,
+  },
+  {
+    skill: "Python",
+    icon: icons.python,
+  },
+  {
+    skill: "Django",
+    icon: icons.django,
+  },
+  {
+    skill: "Tailwind",
+    icon: icons.tailwind,
+  },
+  {
+    skill: "Git",
+    icon: icons.git,
+  },
+  {
+    skill: "GitHub",
+    icon: icons.github,
+  },
+  {
+    skill: "Postman",
+    icon: icons.postman,
   },
 ];
