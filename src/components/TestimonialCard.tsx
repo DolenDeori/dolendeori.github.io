8341a4f33105b815e6648ffe89617cbc8f5f7c13
DolenDeori <dulench.deori564@gmail.com>
2025-06-22 23:12:50 +0530

    feat: Enhance TestimonialCard with animated active state indicator

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,9 +1,12 @@
+import { motion } from "motion/react";
+
 declare interface TestimonialCardProps {
   message: string;
   name: string;
   company: string;
   profile: string;
   className: string;
+  isActive?: boolean;
 }
 
 const TestimonialCard = ({
@@ -12,7 +15,12 @@ const TestimonialCard = ({
   company,
   profile,
   className,
+  isActive,
 }: TestimonialCardProps) => {
+  const radius = 35;
+  const stroke = 3;
+  const circumference = 2 * Math.PI * radius;
+  
   return (
     <>
       <div
@@ -20,7 +28,35 @@ const TestimonialCard = ({
       >
         <p>{message}</p>
         <div className="flex gap-4 items-center justify-center mt-8">
-          <div className="border-3 border-neutral-400 rounded-full p-0.5">
+          <div className="p-2 relative">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex justify-center items-center">
+              {isActive && (
+                <svg
+                  width="100"
+                  height="100"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className=""
+                >
+                  <motion.circle
+                    cx="38"
+                    cy="50"
+                    r={radius}
+                    stroke="#3498db"
+                    strokeWidth={stroke}
+                    fill="none"
+                    strokeDasharray={circumference}
+                    strokeDashoffset={circumference}
+                    animate={{ strokeDashoffset: 0 }}
+                    transition={{
+                      duration: 6,
+                      ease: "easeInOut",
+                      repeat: Infinity,
+                      repeatType: "loop",
+                    }}
+                  />
+                </svg>
+              )}
+            </div>
             <img
               src={profile}
               alt={`${name}'s profile image`}
